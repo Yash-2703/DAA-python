3518cbb2cba65a4cbda8d920a41b79fa262f696b
Yash-2703 <114413384+Yash-2703@users.noreply.github.com>
2023-10-06 21:44:55 +0530

    Divide And Conquer

--- Asg-1 (A-2).py
+++ Asg-1 (A-2).py
@@ -8,13 +8,12 @@ Problem Statement : Implement a problem of move all zeroes to end of array.
 def move_zeros_to_end(arr):
     non_zero_count = 0
 
-    # Iterate through the array, moving non-zero elements to the front.
+    
     for i in range(len(arr)):
         if arr[i] != 0:
             arr[non_zero_count], arr[i] = arr[i], arr[non_zero_count]
             non_zero_count += 1
 
-# Example usage:
 arr = [1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0]
 move_zeros_to_end(arr)
 print(arr)
