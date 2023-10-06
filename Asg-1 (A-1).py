43092d5580ca5df46c3739f5bbae27a6dfe75ac3
Yash <yjondhale780@gmail.com>
2023-10-06 20:49:03 +0530

    Merge remote-tracking branch 'origin/main' into DAA

---
+++ Asg-1 (A-1).py
@@ -0,0 +1,20 @@
+'''---------------------------------------------------------------------------------------------------------------------------------------------
+Problem Statement: Implement a problem of number of zeroes.
+                    -Given an array of 1s and 0s which has all 1s first followed by all 0s? Find the number of 0s.
+                     Count the number of zeroes in the given array.
+---------------------------------------------------------------------------------------------------------------------------------------------'''
+
+
+def count_zeros(arr, left, right):
+    if arr[right] == 1:
+        return 0
+    if arr[left] == 0:
+        return right - left + 1
+
+    mid = (left + right) // 2
+    return count_zeros(arr, left, mid) + count_zeros(arr, mid + 1, right)
+
+# Example usage:
+arr = [1, 1, 1, 1, 0, 0, 0, 0, 0]
+count = count_zeros(arr, 0, len(arr) - 1)
+print("Number of zeros:", count)
--- Asg-1 (A-2).py
+++ Asg-1 (A-2).py
@@ -1,3 +1,10 @@
+'''---------------------------------------------------------------------------------------------------------------------------------------------
+Problem Statement : Implement a problem of move all zeroes to end of array.
+                    - Given an array of random numbers, Push all the zero’s of a given array to the end of the array. For example, if the given
+                      arrays is {1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0}, it should be changed to {1, 9, 8, 4, 2, 7, 6, 0, 0, 0, 0}. The order of all other elements should be same.
+---------------------------------------------------------------------------------------------------------------------------------------------'''
+
+
 def move_zeros_to_end(arr):
     non_zero_count = 0
 
